fd036bd442555a2fcccc04ff0c1a310d4529e44b
Roman Ravas <romorko@gmail.com>
2019-12-30 23:16:30 +0100

    pokec

---
+++ poctySlov.py
@@ -0,0 +1,16 @@
+import _collections
+pocty = _collections.defaultdict(int)
+
+try:
+    with open("text.txt") as file:
+        for line in file:
+            for slovo in line.split():
+                print(slovo)
+                pocty[slovo]+=1
+    for kluc,hodnota in pocty.items():
+        print(kluc,hodnota)
+
+except IOError as ex:
+    print("Subor sa nepodarilo otvorit")
+
+
